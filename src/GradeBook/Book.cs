bd7458807af5f2da716bd0d7b0c4572dcf63e968
Rus <vlad.rus@siemens.com>
2020-04-27 19:20:35 +0300

    included switch statement, for and letter grade

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,10 +11,45 @@ namespace GradeBook
             Name = name;   
         }
      
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+
+                default:
+                    AddGrade(0);   
+                    break;         
+            }
+        }
+        
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >=0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+            
         }
+     
         public Statistics GetStatistics()
         {
             var result = new Statistics();
@@ -22,14 +57,37 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
             
-            foreach(var grade in grades)
+            for(var index = 0; index < grades.Count; index++)
             {
-                result.High= Math.Max(grade, result.Low);
-                result.Low = Math.Min(grade, result.High);
-                result.Average +=grade;
+                result.Low= Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average +=grades[index];
             }
+
             result.Average /= grades.Count;
-            
+
+            switch(result.Average)
+            {
+                case var d when d>= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d>= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d>= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d>= 60.0:
+                    result.Letter = 'D';
+                    break;   
+
+                default:
+                    result.Letter = 'F';
+                    break;             
+            }
             return result;
         }
         private List<double> grades;
